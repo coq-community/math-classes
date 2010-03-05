4cd9894cf88021ff5daf84316318d37c47b5af86
Eelis van der Weegen <eelis@eelis.net>
2010-03-05 13:06:02 +0100

    Add rationale for "fmap".

--- interfaces/functors.v
+++ interfaces/functors.v
@@ -15,6 +15,45 @@ Section functor_class.
 
 End functor_class.
 
+(* The usual notational convention for functor application is to use the
+name of the functor to refer to both its object map and its arrow map, relying
+on additional conventions regarding object/arrow names for disambiguation
+(i.e. "F x" and "F f" map an object and an arrow, respectively, because
+"x" and "f" are conventional names for objects and arrows, respectively.
+
+In Coq, for a term F to function as though it had two different types
+simultaneously (namely the object map and the arrow map), there must
+either (1) be coercions from the type of F to either function, or (2) F must
+be (coercible to) a single function that is able to consume both object and
+arrow arguments. The following snippet shows that (1) doesn't appear to be
+supported in Coq:
+
+  Section test.
+    Context (A B: Type).
+    Record R := { Ra:> A → unit; Rb:> B → unit }.
+    Variables (r: R) (a: A) (b: B).
+    Check (r a). (* ok so far *)
+    Check (r b). (* Error: The term "b" has type "B" while it is expected to have type "A". *)
+  End test.
+
+And even if this /did/ work, we could not use it, because leaving
+computational components unbundled is a key aspect of our approach.
+
+For (2), if it could be made to work at all (which is not clear at all), F would need
+a pretty egregious type considering that arrow types are indexed by objects,
+and that the type of the arrow map (namely "Π x y, (x ⟶ y) → (F x ⟶ F y)")
+must refer to the object map.
+
+We feel that these issues are not limitations of the Coq system, but merely
+reflect the fact that notationally identifying these two different and interdependent
+maps is a typical example of an "abus de notation" that by its very nature
+is ill-suited to a formal development where software engineering concerns apply.
+
+Hence, we do not adopt this practice, and use "fmap F" (name taken from the Haskell
+standard library) to refer to the arrow map of a functor F.
+
+TODO: Sharpen. *)
+
 Section id_functor.
 
   Context `{Category C}.
