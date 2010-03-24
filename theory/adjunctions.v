1d73eed0863a34d002c95aed973075cdfddf3068
Bas Spitters <spitters@cs.ru.nl>
2010-03-24 14:54:42 +0100

    Adding unit and counit of an adjunction.

--- theory/adjunctions.v
+++ theory/adjunctions.v
@@ -3,46 +3,7 @@ Set Automatic Introduction.
 (** We prove the equivalence of the two definitions of adjunction *)
 Require Import
   Relation_Definitions Morphisms Setoid Program abstract_algebra setoids functors categories.
-(*
-Section New_Adjunction.
-  Context `{Category A} `{Category X}
-    (F: X → A) `{!Functor F Fa} (* todo: we don't want to name Fa and Fg here *)
-    (G: A → X) `{!Functor G Ga}
-    (φ: Π {x a}, (F x ⟶ a) → (x ⟶ G a))
-    `{Π x a, Inv (φ x a)}
-    `{Π x a, Bijective (φ x a)}
-    (φ_adj:Adjunction _ _ φ).
-Implicit Arguments φ [[x] [a]].
-(* The definition of adjunction in terms of arrows, i.e. using the right_adjunct *)
-Context `{f:F x ⟶ a}.
-Lemma rad_l `(k:a  ⟶ a'): φ (k ◎ f)= (fmap G k) ◎ φ f.
-
-Admitted.
-Lemma rad_r `(h:x' ⟶ x): φ (f ◎ fmap F h) = φ f ◎ h.
-Admitted.
-
-
-  Context `{Category C} `{Category D}
-    `{!Functor (F: C → D) F'} (* todo: we don't want to name F' and F' here *)
-    `{!Functor (G: D → C) G'}
-    (η: id ⇛ G ∘ F) `{!NaturalTransformation η}
-    (φ: Π (x: C) (y: D) (f: x ⟶ G y), F x ⟶ y).
-
-  Context `{Category A} `{Category X}
-    (F: X → A) `{!Functor F} (* todo: we don't want to name Fa and Fg here *)
-    (G: A → X) `{!Functor G Ga}
-    (φ: Π {x a}, (F x ⟶ a) → (x ⟶ G a))
-    (φinv: Π {x a}, Inv (@φ x a))
-(*Can we make the inverse implicit??*)
-    `{Π x a, Bijective' (φ x a) (*(φinv x a)*)}.
-
-  Class Adjunction': Prop :=
-   { natural_left `(k: a ⟶ a'): `((fmap G k ◎) ∘ φ = @φ x _ ∘ (k ◎))
-   ; natural_right `(h: x' ⟶ x):`((◎ h) ∘ @φ _ a = φ ∘ (◎ fmap F h)) }.
-
-End New_Adjunction.
-*)
-Section adj2alt_adj.
+Section unit.
 
 Context `{Category A} `{Category X}
     (F: X → A) `{!Functor F Fa} (* todo: we don't want to name Fa and Fg here *)
@@ -67,25 +28,20 @@ Instance: ∀ x y, Setoid_Morphism (@φ x y).
  apply _.
 Qed.
 
-Instance: NaturalTransformation η.
+Instance eta: NaturalTransformation η.
  unfold NaturalTransformation. 
  (* MacLane p81 *)
  unfold η. intros x' x h.
  symmetry.
  setoid_replace (fmap (G ∘ F) h ◎ φ cat_id) with (φ (fmap F h ◎ cat_id)).
-  Focus 2.
-  symmetry.
-  rewrite rad_l; [| apply _].
-  cut (fmap G (fmap F h)=  fmap (G ∘ F) h).
-   intro. rewrite H5. reflexivity.
+  2: symmetry; rewrite rad_l; [| apply _].
+  Focus 2. cut (fmap G (fmap F h)=  fmap (G ∘ F) h).
+   (intro H5; rewrite H5; reflexivity).
   symmetry.
    (* apply preserves_comp. TODO: Prove for normal arrow in Sets *) admit.
-  setoid_replace (φ (fmap F h ◎ cat_id)) with (φ ( cat_id ◎ (fmap F h))).
-Focus 2. transitivity (φ (fmap F h)). 
-rewrite id_r.
-(* TODO: add Hint that bijective is a Steoid_Morphism. *)
-reflexivity.
-rewrite id_l. reflexivity.
+  setoid_replace (φ (fmap F h ◎ cat_id)) with (φ ( cat_id ◎ (fmap F h))) by
+(transitivity (φ (fmap F h)); [rewrite id_r;reflexivity|rewrite id_l; reflexivity]).
+(* TODO: add Hint that bijective is a Setoid_Morphism. *)
 setoid_replace (φ (cat_id ◎ fmap F h)) with (φ cat_id ◎ fmap id h) by apply (rad_r F G).
 reflexivity. apply _.
 Qed.
@@ -99,7 +55,7 @@ GF h ∘ (φ (cat_id (F x')))=(φ (fmap F h∘ cat_id (F x')))
 Based on the naturality of φ.
 *)
 
-
+(** Characterization of φ in terms of G and η *)
 Lemma φarrows `(f:(F x)  ⟶ a):φ f = (fmap G f)  ◎ (η x).
 Proof with reflexivity.
 rewrite <- (id_r  f).
@@ -107,60 +63,77 @@ rewrite <- (id_r  f).
 rewrite  rad_l by apply _.
 rewrite id_r...
 Qed.
+End unit.
 
-Definition ϵ  : F ∘ G ⇛ id :=
-  (λ a:A => (@inv _  _ (@φ (G a) a) _ (@cat_id X _ _ (G a)))).
+Require Import dual.
 
-(*
-Instance: ∀ x y, Setoid_Morphism (@inv _  _ (@φ x y)).
- intros.
- destruct (H4 x y).
- destruct bijective_injective.
- apply _.
+Section opfunctor.
+(** Given a functor F:C->D, we have a functor F^op:C^op -> D^op *)
+Context `{Category A} `{Category X}
+    (F: X → A) `{!Functor F Fa}.
+Definition functor_op (func:Fmap F)  :(@Fmap _ flipA _ flipA F):=
+fun v w f => func w v f.
+Implicit Arguments functor_op [[func]].
+
+Instance :Functor F functor_op.
+unfold e, functor_op, flipA, flip, CatId_instance_0, CatComp_instance_0, flip. constructor. intuition.
+      destruct (functor_morphism F b a); constructor; intuition.
+   intro a. set (preserves_id F a);intuition.
+  intros. unfold fmap. apply (@preserves_comp _ _ H1 _ _ _ _ H _ _ F);assumption.
 Qed.
-*)
+End opfunctor.
 
-Instance: NaturalTransformation ϵ.
- unfold NaturalTransformation. 
- (* MacLane p82 *)
- unfold ϵ. intros a' a k.
- symmetry. 
-(* ETC ... Can we use the op-cat?? 
+Section counit.
+Context `{Category A} `{Category X}
+    (F: X → A) `{!Functor F Fa} (* todo: we don't want to name Fa and Fg here *)
+    (G: A → X) `{!Functor G Ga}
+    (φ: Π {x a}, (F x ⟶ a) → (x ⟶ G a))
+    `{Π x a, Inv (φ x a)}
+    `{Π x a, Bijective (φ x a)}
+    (φ_adj:Adjunction _ _ φ).
+Notation "f ⁻¹" := (@inv _ _ f _) (at level 30).
+Definition φinv:=λ x a => (@φ x a)⁻¹.
 
- setoid_replace (fmap (G ∘ F) h ◎ φ cat_id) with (φ (fmap F h ◎ cat_id)).
-  Focus 2.
-  symmetry.
-  rewrite rad_l; [| apply _].
-  cut (fmap G (fmap F h)=  fmap (G ∘ F) h).
-   intro. rewrite H5. reflexivity.
-  symmetry.
-   (* apply preserves_comp. TODO: Prove for normal arrow in Sets *) admit.
-  setoid_replace (φ (fmap F h ◎ cat_id)) with (φ ( cat_id ◎ (fmap F h))).
-Focus 2. transitivity (φ (fmap F h)). 
-rewrite id_r.
-(* TODO: add Hint that bijective is a Steoid_Morphism. *)
-reflexivity.
-rewrite id_l. reflexivity.
-setoid_replace (φ (cat_id ◎ fmap F h)) with (φ cat_id ◎ fmap id h) by apply (rad_r F G).
-reflexivity. apply _.
-Qed.
+(** And an adjunction *)
 
-(**
-It is a natural transformation because:
-GF h ∘ (φ (cat_id (F x')))=(φ (fmap F h∘ cat_id (F x')))
-   = φ ( cat_id (F x) ∘ (fmap F h))
-   = φ ( cat_id (F x) ∘ h
+Require Import bijective.
+Definition inverse0: ∀x : A, ∀a : X, Inv ((fun (a0 : A) (x0 : X) => φinv x0 a0) x a):=
+fun (a : A) (x : X) => φ x a. 
 
-Based on the naturality of φ.
-*)
-
-
-Lemma φarrows `(f:(F x)  ⟶ a):φ f = (fmap G f)  ◎ (η x).
-Proof with reflexivity.
-rewrite <- (id_r  f).
-(* TODO: Finish the proof by autorewrite *)
-rewrite  rad_l by apply _.
-rewrite id_r...
+Lemma inverse: (∀x : A,
+        ∀a : X,
+        @Bijective (G x ⟶ a) (@e X Arrows1 H1 (G x) a) (x ⟶ F a)
+          (@e A Arrows0 H x (F a))
+          ((fun (a0 : A) (x0 : X) => φinv x0 a0) x a) (inverse0 x a)).
+intros a x. unfold φinv, e, inverse0. apply (@invBij _ _ _ _ _ _ (φ x a)); auto.
 Qed.
 
+Instance op_adj:(@Adjunction _ _ _ _ _  _ _  _ (functor_op G _)  _ ( functor_op F _) 
+  (λ a x => (@φ x a)⁻¹ )). (* flip *)
+  destruct φ_adj. 
+  constructor. 
+    intros x x' k a h. set (g:=((φ x a ⁻¹) h)).
+   assert ((φ x' a) (CatComp0 (F x') (F x) a g (Fa x' x k)) = (CatComp1 x' x (G a) ((φ x a) g) k)) by
+      (symmetry; apply natural_right).
+    unfold comp, compose, φinv, CatComp_instance_0, flip, flipA, flip, functor_op, fmap, e. 
+    revert H5. set (t1:=(CatComp0 (F x') (F x) a g (Fa x' x k))).
+    subst g. rewrite (back' (H4 x a) h). (* hangs on: intro r.  rewrite <- r *)
+    apply cancel_left. 
+  clear natural_right. intros a a' h x k. set (l:=((φ x a' ⁻¹) k)).
+  assert ((φ x a) (CatComp0 (F x) a' a h l) = (CatComp1 x (G a') (G a) (Ga a' a h) (φ x a' l))) by
+      (symmetry; apply natural_left).
+  unfold comp, compose, φinv, CatComp_instance_0, flip, flipA, flip, functor_op, fmap, e. 
+  revert H5. set (t1:=(CatComp0 (F x) a' a h l)). unfold l.
+   rewrite (back' (H4 x a') k). apply cancel_left.
+Qed.
+
+Definition ϵnat := (@eta X (@flipA X Arrows1) (@e X Arrows1 H1)
+  (@CatId_instance_0 X Arrows1 CatId1)
+  (@CatComp_instance_0 X Arrows1 CatComp1)
+  (@cat X Arrows1 H1 CatId1 CatComp1 H2) A (@flipA A Arrows0)
+  (@e A Arrows0 H) (@CatId_instance_0 A Arrows0 CatId0)
+  (@CatComp_instance_0 A Arrows0 CatComp0)
+  (@cat A Arrows0 H CatId0 CatComp0 H0) G (functor_op G Ga) F (functor_op F Fa) 
+  (fun (a : A) (x : X) => φinv x a) _ inverse  op_adj).
+End counit.
 
