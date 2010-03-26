185a2b5c5a6960b3da7724ce71a7b03e6ba397e5
Eelis van der Weegen <eelis@eelis.net>
2010-03-26 22:59:56 +0100

    Add empty and setoid varieties.

---
+++ varieties/empty.v
@@ -0,0 +1,31 @@
+Set Automatic Introduction.
+
+Require
+  theory.rings categories.variety.
+Require Import
+  Program Morphisms
+  abstract_algebra universal_algebra.
+
+Definition op := False.
+
+Definition sig: Signature := Build_Signature False False (False_rect _).
+
+Definition Laws (_: EqEntailment sig): Prop := False.
+
+Definition theory: EquationalTheory := Build_EquationalTheory sig Laws.
+
+Let carriers := False_rect _: sorts sig → Type.
+
+Instance: Equiv (carriers a).
+Proof. intros []. Qed.
+
+Instance implementation: AlgebraOps sig carriers := λ o => False_rect _ o.
+
+Global Instance: Algebra sig _.
+Proof. constructor; intuition. Qed.
+
+Instance variety: InVariety theory carriers.
+Proof. constructor; intuition. Qed.
+
+Definition Object := variety.Object theory.
+Definition object: Object := variety.object theory carriers.
---
+++ varieties/setoid.v
@@ -0,0 +1,57 @@
+Set Automatic Introduction.
+
+Require
+  theory.rings categories.variety.
+Require Import
+  Program Morphisms
+  abstract_algebra universal_algebra.
+
+Definition op := False.
+
+Definition sig: Signature := Build_Signature unit op (False_rect _).
+
+Definition Laws: EqEntailment sig → Prop := λ _ => False.
+
+Definition theory: EquationalTheory := Build_EquationalTheory sig Laws.
+
+(* Constructing an object in the variety from an instance of the class: *)
+
+Section from_instance.
+
+  Context A `{Setoid A}.
+
+  Notation carriers := (λ _ => A).
+
+  Instance: AlgebraOps sig carriers := λ o => False_rect _ o.
+
+  Instance: Algebra sig carriers.
+  Proof. constructor; intuition. Qed.
+
+  Instance: InVariety theory carriers.
+  Proof. constructor; intuition. Qed.
+
+  Definition object: variety.Object theory := variety.object theory (λ _ => A).
+
+End from_instance.
+
+(* Constructing an instance of the class from an object in the variety: *)
+
+Section from_variety.
+
+  Context `{InVariety theory A}.
+
+  Instance: Setoid (A tt).
+
+End from_variety.
+
+Lemma mor_from_sr_to_alg `{InVariety theory A} `{InVariety theory B}
+  (f: Π u, A u → B u) `{!Setoid_Morphism (f tt)}: HomoMorphism sig A B f.
+Proof with try apply _.
+ constructor.
+    intros []...
+   intros [].
+  change (Algebra theory A)...
+ change (Algebra theory B)...
+Qed.
+
+Instance struct_from_var_to_class `{InVariety theory A}: Setoid (A tt).
