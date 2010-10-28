b6021cf1595f28bd3bbdea1c00a74dfad0b486b9
Robbert Krebbers <mail@robbertkrebbers.nl>
2010-10-28 10:58:56 +0200

    Implementation of the dyadics finished

--- src/implementations/dyadics.v
+++ src/implementations/dyadics.v
@@ -1,21 +1,33 @@
-Require rings.
+(** 
+  The dyadic rationals are numbers of the shape
+  [z # 2 ^ n] with [z : Z] and [n : N] for some [Integers] implementation [Z] and
+  [Naturals] implementation [N]. These numbers form a ring and can be embedded
+  into any [Rationals] implementation [Q]. 
+*)
+
 Require Import
   Morphisms Ring Program RelationClasses
-  interfaces.integers interfaces.naturals
   abstract_algebra 
   canonical_names 
+  interfaces.integers interfaces.naturals interfaces.rationals
   interfaces.bit_shift
-  rationals
-  theory.integers.
+  theory.integers theory.rings theory.fields.
 
 Section dyadics.
-  Context Z N `{Integers Z} `{Naturals N} `{sl : !ShiftLeft Z N}.
-  
+  Context Z N `{Integers Z} `{Naturals N}.
   Add Ring Z: (rings.stdlib_ring_theory Z).
   Add Ring N: (rings.stdlib_semiring_theory N).
 
-  Record Dyadic := dyadic { mant: Z; expo: N }.
+  (* Without this Instance many rewrites result in an seeminly infinite loop... *)
+  Context `{sl : !ShiftLeft Z N}.
+  Instance: Proper ((=) ==> (=) ==> (=)) (@shiftl Z N _ _ _ _ _ _). 
+  Proof.  apply _. Qed.
 
+  (** * Definition *)
+  Record Dyadic := dyadic { mant: Z; expo: N }.
+  Infix "#" := dyadic (at level 80).
+
+  (** * Equality *)
   Global Instance dy_eq: Equiv Dyadic := λ x y,
     mant y ≪ expo x = mant x ≪ expo y.
   
@@ -24,33 +36,18 @@ Section dyadics.
     unfold equiv, dy_eq. apply _. 
   Defined.
 
-  Global Instance dy_mult: RingMult Dyadic := λ x y, dyadic 
-    (mant x * mant y) 
-    (expo x + expo y).
-
-  Global Instance dy_plus: RingPlus Dyadic := λ x y, dyadic
-    (mant y ≪ expo x + mant x ≪ expo y)
-    (expo x + expo y).
-
-  Global Instance dy_opp: GroupInv Dyadic := λ x, dyadic (- mant x) (expo x).
-  Global Instance dy_0: RingZero Dyadic := dyadic 0 0.
-  Global Instance dy_1: RingOne Dyadic := dyadic 1 0.
-
-  Global Instance: Reflexive (_ : Equiv Dyadic).
+  Global Instance: Reflexive dy_eq.
   Proof.
     intro. unfold equiv, dy_eq. reflexivity.
   Qed.
 
-  Global Instance: Symmetric (_ : Equiv Dyadic).
+  Global Instance: Symmetric dy_eq.
   Proof.
-    intros x y E. unfold equiv, dy_eq in *. rewrite E. reflexivity.
+    intros x y E. unfold equiv, dy_eq in *. 
+    rewrite E. reflexivity.
   Qed.
 
-  (* Without this Instance many rewrites result in a loop... *)
-  Instance: Proper ((=) ==> (=) ==> (=)) (@shiftl Z N _ _ _ _ _ _). 
-  Proof.  apply _. Qed.
-
-  Global Instance: Transitive (_ : Equiv Dyadic).
+  Global Instance: Transitive dy_eq.
   Proof with eauto; try reflexivity.
     intros x y z E1 E2. unfold equiv, dy_eq in *.
     apply shiftl_inj with (expo y).
@@ -58,19 +55,24 @@ Section dyadics.
     rewrite shiftl_order. Timeout 5 rewrite E2. 
     rewrite (shiftl_order _ (expo z) (expo y)). rewrite <-E1. 
     rewrite shiftl_order...
-    (* transitivity (mant y ≪ expo z ≪ expo x). 
-    apply shiftl_proper... 
-    rewrite (shiftl_order (mant y)).
-    transitivity (mant x ≪ expo y ≪ expo z).
-    apply shiftl_proper...
-    apply shiftl_order. *)
   Qed.
 
-  Global Instance: Equivalence (_ : Equiv Dyadic).
+  Global Instance: Equivalence dy_eq.
   Global Instance: Setoid Dyadic.
  
+  (** * Basic operations *)
+  Global Instance dy_mult: RingMult Dyadic := λ x y,  
+    mant x * mant y # expo x + expo y.
+
+  Global Instance dy_plus: RingPlus Dyadic := λ x y,
+    mant y ≪ expo x + mant x ≪ expo y # expo x + expo y.
+
+  Global Instance dy_opp: GroupInv Dyadic := λ x, -mant x # expo x.
+  Global Instance dy_0: RingZero Dyadic := 0 # 0.
+  Global Instance dy_1: RingOne Dyadic := 1 # 0.
+
   (* the Timeouts are present to detect possible hangs caused by rewrite *)
-  Global Instance: Associative (_ : RingPlus Dyadic).
+  Global Instance: Associative dy_plus.
   Proof with auto; try ring.
     repeat intro. unfold dy_plus, equiv, dy_eq. simpl. 
     apply shiftl_proper...
@@ -88,7 +90,7 @@ Section dyadics.
     apply shiftl_proper... 
   Qed. 
 
-  Global Instance: Proper ((=) ==> (=) ==> (=)) (_ : RingPlus Dyadic).
+  Global Instance: Proper ((=) ==> (=) ==> (=)) dy_plus.
   Proof with try reflexivity; auto.
     intros x1 y1 E1 x2 y2 E2. unfold equiv, dy_eq in *. unfold dy_plus. simpl.
     Timeout 5 repeat rewrite shiftl_sum_base.
@@ -96,37 +98,21 @@ Section dyadics.
     apply sg_mor.
     rewrite shiftl_order_4a. rewrite E2. rewrite shiftl_order_4b...
     rewrite shiftl_order_4b. rewrite E1. rewrite (shiftl_order (mant x1))...
-    (*  assert (mant y2 ≪ expo y1 ≪ expo x1 ≪ expo x2 = mant x2 ≪ expo y2 ≪ expo y1 ≪ expo x1) as E3.
-      Timeout 5 rewrite (shiftl_order _ (expo x1) (expo x2))...
-      Timeout 5 rewrite (shiftl_order _ (expo y1) (expo x2))...
-    rewrite E3. clear E3.
-    Timeout 5 rewrite (shiftl_order _ (expo y1) (expo x1)).
-    Timeout 5 rewrite (shiftl_order _ (expo y1) (expo y2))...
-    Timeout 5 rewrite (shiftl_order _ (expo y2) (expo x1))...
-    assert (mant y1 ≪ expo y2 ≪ expo x1 ≪ expo x2 = mant x1 ≪ expo y1 ≪ expo y2 ≪ expo x2) as E3.
-      apply shiftl_proper; try reflexivity.
-      Timeout 5 rewrite (shiftl_order _ (expo y2) (expo x1))...
-    rewrite E3. clear E3.
-    Timeout 5 rewrite (shiftl_order _ (expo y2) (expo x2))...
-    Timeout 5 rewrite (shiftl_order _ (expo y1) (expo x2))... *)
   Qed.
 
-  Global Instance: SemiGroup Dyadic (op:=(_ : RingPlus Dyadic)).
+  Global Instance: SemiGroup Dyadic (op:=dy_plus).
 
   Lemma dyadic_left_identity (x : Dyadic) : 0 + x = x.
   Proof with try reflexivity.
     unfold equiv, dy_eq, sg_op, dy_plus. simpl.
     rewrite left_identity. rewrite right_identity. rewrite left_absorb. rewrite right_identity...
-    (* rewrite shiftl_sum_exp.
-    apply shiftl_proper...
-    rewrite right_identity, left_absorb, right_identity... *)
   Qed.
 
-  Global Program Instance: Monoid Dyadic (op:=(_ : RingPlus Dyadic)) (unit:=(_ : RingZero Dyadic)).
+  Global Program Instance: Monoid Dyadic (op:=dy_plus) (unit:=dy_0).
   Next Obligation. repeat intro. apply dyadic_left_identity. Qed.
   Next Obligation. repeat intro. rewrite commutativity. apply dyadic_left_identity. Qed.
 
-  Global Instance: Proper ((=) ==> (=)) (_: GroupInv Dyadic).
+  Global Instance: Proper ((=) ==> (=)) dy_opp.
   Proof.
     intros x y E.
     unfold equiv, dy_eq, dy_opp in *. simpl.
@@ -141,10 +127,6 @@ Section dyadics.
     rewrite <- shiftl_sum_base.
     assert (mant x + - mant x = 0) as E by ring. rewrite E.
     rewrite left_absorb...
-    (* assert ((mant x + - mant x) ≪ expo x = 0 ≪ expo x) as E.
-      apply shiftl_proper... ring.  
-    rewrite E. clear E.
-    rewrite left_absorb... *)
   Qed.
 
   Global Program Instance: Group Dyadic.
@@ -153,19 +135,19 @@ Section dyadics.
 
   Global Program Instance: AbGroup Dyadic.
 
-  Global Instance: Associative (_: RingMult Dyadic).
+  Global Instance: Associative dy_mult.
   Proof.
    repeat intro. unfold ring_mult, dy_mult, equiv, dy_eq.
    simpl. apply shiftl_proper; ring.
   Qed.
 
-  Global Instance: Commutative (_: RingMult Dyadic).
+  Global Instance: Commutative dy_mult.
   Proof.
    repeat intro. unfold ring_mult, dy_mult, equiv, dy_eq.
    simpl. apply shiftl_proper; ring.
   Qed.
 
-  Global Instance: Proper ((=) ==> (=) ==> (=)) (_: RingMult Dyadic).
+  Global Instance: Proper ((=) ==> (=) ==> (=)) dy_mult.
   Proof with intuition.
     intros x1 y1 E1 x2 y2 E2.
     unfold equiv, dy_eq, dy_mult in *.  simpl.
@@ -177,26 +159,21 @@ Section dyadics.
     rewrite shiftl_order. rewrite commutativity...
   Qed.
 
-  Global Instance: SemiGroup Dyadic (op:=(_: RingMult Dyadic)).
+  Global Instance: SemiGroup Dyadic (op:=dy_mult).
 
   Lemma dyadic_mult_left_identity (x : Dyadic) : 1 * x = x.
   Proof with try reflexivity.
     unfold equiv, dy_eq, sg_op, dy_plus. simpl.
     rewrite left_identity. rewrite left_identity... 
-    (* intro; unfold equiv, dy_eq, sg_op, dy_mult; simpl.
-    rewrite 
-    rewrite shiftl_sum_exp.
-    apply shiftl_proper...
-    rewrite left_identity, right_identity... *)
   Qed.
 
-  Global Program Instance: Monoid Dyadic (op:=(_: RingMult Dyadic)) (unit:=(_: RingOne Dyadic)).
+  Global Program Instance: Monoid Dyadic (op:=dy_mult) (unit:=dy_1).
   Next Obligation. repeat intro. apply dyadic_mult_left_identity. Qed.
   Next Obligation. repeat intro. rewrite commutativity. apply dyadic_mult_left_identity. Qed.
 
-  Global Instance: CommutativeMonoid Dyadic (op:=(_: RingMult Dyadic)) (unit:=(_: RingOne Dyadic)).
+  Global Instance: CommutativeMonoid Dyadic (op:=dy_mult) (unit:=dy_1).
 
-  Global Instance: Distribute (_: RingMult Dyadic) (_: RingPlus Dyadic).
+  Global Instance: Distribute dy_mult dy_plus.
   Proof with try reflexivity.
     split; intros x y z; unfold equiv, dy_eq, dy_mult, dy_plus; simpl.
 
@@ -221,66 +198,143 @@ Section dyadics.
 
   Global Instance: Ring Dyadic.
 
-  (* now if we already have a rationals implementation... *)
-  Context `{Rationals Q} (ZtoQ: Z → Q) `{!Ring_Morphism ZtoQ} `{FieldDiv Q}.
+  (** 
+   * Embedding into the rationals
+   If we already have a [Rationals] implementation [Q], then we can embed [Dyadic]
+   into it. That is, we have an injective ring morphism [DtoQ : Dyadic → Q].
+  *)
+  Context `{Rationals Q}.
+  Add Field Q: (stdlib_field_theory Q).
 
   (* We don't make (Z >-> Q) a coercion because it could be computationally expensive
    and we want to see where it's called. *)
-  Require Import theory.rings.
-  Program Definition inject (d: Dyadic): Q := ZtoQ (mant d) * // ZtoQ ((1+1) ≪ expo d).
-  Next Obligation with auto.
-   intro.
-   apply (shiftl_nonzero (1+1) (expo d)). 
-   exact two_nonzero.
-   apply (injective ZtoQ).
-   rewrite preserves_0...
+  Context (ZtoQ: Z → Q) `{!Ring_Morphism ZtoQ}.
+  
+  (* We use binary division because that might have a more efficient implementation. *)
+  Context `{fd : !FieldDiv Q}.
+
+  Program Definition EtoQ (n : N) : { z : Q | z ≠ 0 } := ZtoQ (1 ≪ n).
+  Next Obligation with intuition.
+    intros E.
+    apply (shiftl_nonzero 1 n).
+    intro E2. symmetry in E2. apply (zero_ne_one E2).
+    apply (injective ZtoQ).
+    rewrite preserves_0...
   Qed.
 
-  Instance: Proper ((=) ==> (=)) inject.
+  Global Instance EtoQ_proper: Proper ((=) ==> (=)) EtoQ.
+  Proof.
+    intros x y E.
+    unfold EtoQ. unfold equiv, sig_equiv, sig_relation. simpl.
+    rewrite E. reflexivity.
+  Qed.
+ 
+  Definition DtoQ (d: Dyadic): Q := ZtoQ (mant d) / EtoQ (expo d).
+
+  Global Instance: Proper ((=) ==> (=)) DtoQ.
   Proof with try apply _.
-   intros x y E.
-   unfold inject. 
-   apply theory.fields.equal_quotients. simpl.
-   do 2 rewrite shiftl_sum_base.
-   do 2 rewrite <- preserves_mult.
-   do 2 rewrite distribute_l.
-   do 2 rewrite mult_shiftl.
-   do 2 rewrite right_identity.
-   unfold equiv, dy_eq in E. rewrite E.
-   reflexivity.
+    intros x y E.
+    unfold DtoQ, EtoQ.
+    do 2 rewrite field_div_correct.
+    apply equal_quotients. simpl.
+    do 2 rewrite <- preserves_mult.
+    do 2 rewrite mult_shiftl.
+    do 2 rewrite right_identity.
+    unfold equiv, dy_eq in E. rewrite E.
+    reflexivity.
   Qed.
 
-  Lemma inject_0: inject 0 = 0.
-  Proof. unfold inject. simpl. rewrite preserves_0. apply left_absorb. Qed.
+  Lemma EtoQ_plus_mult x y :  EtoQ (x + y) = (EtoQ x) * (EtoQ y).
+  Proof.
+    intros.
+    unfold equiv, sig_equiv, sig_relation, EtoQ. simpl.
+    rewrite <-preserves_mult.
+    rewrite <-mult_shiftl_1.
+    rewrite shiftl_sum_exp.
+    reflexivity.
+  Qed.
 
-  Instance: Ring_Morphism inject.
-  Proof with auto.
+  Lemma EtoQ_zero_one : EtoQ 0 = 1.
+  Proof.
+    unfold equiv, sig_equiv, sig_relation, EtoQ. simpl.
+    rewrite right_identity.
+    rewrite preserves_1.
+    reflexivity.
+  Qed.
+
+  Lemma DtoQ_quotients a b c d : 
+    ZtoQ a / EtoQ b + ZtoQ c / EtoQ d = ZtoQ (a ≪ d + c ≪ b) / EtoQ (b + d).
+  Proof.
+    rewrite preserves_plus.
+    do 3 rewrite field_div_correct.
+    rewrite quotients. simpl.
+    repeat rewrite <-preserves_mult.
+    repeat rewrite <-mult_shiftl_1.
+    rewrite EtoQ_plus_mult. 
+    reflexivity.
+  Qed.
+
+  Lemma DtoQ_preserves_plus x y : DtoQ (x + y) = DtoQ x + DtoQ y.
+  Proof.
+    unfold ring_plus at 1. unfold dy_plus at 1.
+    unfold DtoQ. simpl.
+    rewrite (commutativity (mant y ≪ expo x) _).
+    symmetry. apply DtoQ_quotients.
+  Qed.
+  
+  Lemma DtoQ_preserves_zero : DtoQ 0 = 0.
+  Proof. 
+    unfold DtoQ. simpl.
+    rewrite field_div_correct. 
+    rewrite preserves_0. ring.
+  Qed.
+  
+  Lemma DtoQ_preserves_group_inv x : DtoQ (-x) = -DtoQ x.
+  Proof. 
+    unfold DtoQ. simpl.
+    do 2 rewrite field_div_correct.
+    rewrite preserves_inv. ring.
+  Qed.
+
+  Lemma DtoQ_preserves_mult x y : DtoQ (x * y) = DtoQ x * DtoQ y.
+  Proof. 
+    unfold ring_mult at 1. unfold dy_mult at 1.
+    unfold DtoQ. simpl.
+    do 3 rewrite field_div_correct. 
+    rewrite preserves_mult.
+    rewrite EtoQ_plus_mult. 
+    rewrite <-mult_inv_distr.
+    ring.
+  Qed.
+
+  Lemma DtoQ_preserves_one : DtoQ 1 = 1.
+  Proof. 
+    unfold DtoQ. simpl.
+    rewrite field_div_correct.
+    rewrite preserves_1. rewrite EtoQ_zero_one.
+    apply mult_inverse'.
+  Qed.
+
+  Instance: Ring_Morphism DtoQ.
+  Proof. 
     repeat (split; try apply _).
-    intros x y.
-    unfold inject. simpl. 
-  Admitted.
+    exact DtoQ_preserves_plus.
+    exact DtoQ_preserves_zero.
+    exact DtoQ_preserves_group_inv.
+    exact DtoQ_preserves_mult.
+    exact DtoQ_preserves_one.
+  Qed.
 
-  Instance: Injective inject.
+  Instance: Injective DtoQ.
   Proof with auto.
-   repeat intro.
-   unfold equiv, dy_eq.
-  Admitted. (*
-   apply (injective ZtoQ).
-   
-   apply (theory.fields.equal_quotients) in H5.
-   simpl in H5.
-   unfold den in H5.
-   unfold compose in H5.
-   rewrite <- mult_shiftl_1.
-   rewrite <- (mult_shiftl_1 (num x)).
-   apply (theory.fields.equal_quotients) in H4.
-   do 2 rewrite preserves_mult.
-   symmetry. assumption.
-  Qed.*)
+    split; try apply _.
+    intros x y E.
+    unfold equiv, dy_eq. unfold DtoQ in E. unfold EtoQ in E.
+    do 2 rewrite field_div_correct in E.
+    apply equal_quotients in E. simpl in E.
+    do 2 rewrite <-preserves_mult in E.
+    do 2 rewrite <-mult_shiftl_1 in E.
+    symmetry. apply (injective ZtoQ)...
+  Qed.
 
-  
-
-  
-
-
-End contents.
+End dyadics.
