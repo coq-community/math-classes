4f92a68662eaf55cb4c6dad26c13105db42ed688
Eelis van der Weegen <eelis@eelis.net>
2010-12-01 04:09:23 +0100

    Put Coq SCons builder in site_scons.

---
+++ src/site_scons/site_tools/Coq.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import SCons.Defaults, SCons.Tool, SCons.Util, os
+
+def add_glob(target, source, env):
+  base, _ = os.path.splitext(str(target[0]))
+  target.append(base + ".glob")
+  return target, source
+
+Coq = SCons.Builder.Builder(
+  action = 'coqc $COQFLAGS -q  $SOURCE',
+  suffix = '.vo',
+  src_suffix = '.v',
+  emitter = add_glob)
+
+def generate(env):
+  env.Append(BUILDERS = {'Coq': Coq})
+
+def exists(env):
+  return env.Detect('coqc')
+
