42eb464a07c259c4d23b59c814aac77035c09b47
Robbert Krebbers <mail@robbertkrebbers.nl>
2011-04-18 22:07:16 +0200

    Fix vectorspace.

--- src/interfaces/vectorspace.v
+++ src/interfaces/vectorspace.v
@@ -11,7 +11,7 @@ Notation "(· x )" := (λ y, y · x) (only parsing).
 
 Class VectorSpace (F G : Type) {eF plusF multF zeroF oneF oppF invF}
      {eG opG unitG invG} {sm : ScalarMult F G} := {
-  vs_field :> @Field F eF plusF multF zeroF oneF oppF invF ;
+  vs_field :> @DecField F eF plusF multF zeroF oneF oppF invF ;
   vs_abgroup :> @AbGroup G eG opG unitG invG ;
   vs_distr_l :> LeftHeteroDistribute (·) (&) (&) ;
   vs_distr_r :> RightHeteroDistribute (·) (+) (&) ;
