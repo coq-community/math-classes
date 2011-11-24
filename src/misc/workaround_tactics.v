e3b0471abc1f0f64e1e6a08982ff788ac9b0c6dc
Tom Prince <tom.prince@ualberta.net>
2011-11-24 01:11:15 -0500

    Use ltac's fresh, to avoid variable capture in tactics.

--- src/misc/workaround_tactics.v
+++ src/misc/workaround_tactics.v
@@ -1,4 +1,4 @@
-Global Ltac posed_rewrite t := pose proof t as TEMP; rewrite TEMP; clear TEMP.
+Global Ltac posed_rewrite t := let TEMP:=fresh in pose proof t as TEMP; simpl TEMP; rewrite TEMP; clear TEMP.
   (* Workaround around Coq bug, probably same as #2185. *)
-Global Ltac apply_simplified x := generalize x; simpl; intro HHH; apply HHH; clear HHH.
-Tactic Notation "posed_rewrite" "<-" constr(t) := pose proof t as TEMP; rewrite <-TEMP; clear TEMP.
+Global Ltac apply_simplified x := let TEMP:=fresh in generalize x; simpl; intro TEMP; apply TEMP; clear TEMP.
+Tactic Notation "posed_rewrite" "<-" constr(t) := let TEMP:= fresh in pose proof t as TEMP; simpl TEMP; rewrite <-TEMP; clear TEMP.
