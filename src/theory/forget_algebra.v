8a41c8b5a2f5c6b66dda0934a5d45b7fbfba791e
Tom Prince <tom.prince@ualberta.net>
2011-11-24 13:18:44 -0500

    Simplify theory/forget_algebra.

--- src/theory/forget_algebra.v
+++ src/theory/forget_algebra.v
@@ -4,7 +4,8 @@ This functor should nicely compose with the one forgetting variety laws. *)
 
 Require Import
   Morphisms Coq.Setoids.Setoid abstract_algebra universal_algebra interfaces.functors
-  ua_homomorphisms theory.categories
+  ua_homomorphisms theory.categories.
+Require
   categories.setoids categories.product categories.algebras.
 
 Section contents.
@@ -12,48 +13,33 @@ Section contents.
 
   Notation TargetObject := (product.Object (λ _: sorts sign, setoids.Object)).
 
-  Let TargetArrows: Arrows TargetObject := @product.pa _ (λ _: sorts sign, setoids.Object) (λ _, _: Arrows setoids.Object).
-    (* hm, not happy about this *)
-
-  Definition object (v: algebras.Object sign): TargetObject := λ i, setoids.object (v i) (algebras.algebra_equiv sign v i) _.
+  Definition object (v: algebras.Object sign): TargetObject := λ i, setoids.object (v i) (algebras.algebra_equiv sign v i).
 
   Global Program Instance: Fmap object := λ _ _, id.
-  Next Obligation. destruct x. simpl. apply _. Qed.
+  Next Obligation. apply _. Qed.
 
   Global Instance forget: Functor object _.
-  Proof with intuition.
+  Proof.
    constructor; try apply _.
-     constructor; try apply _.
-     intros x y E i A B F. simpl in *.
-     unfold id.
-     destruct (@homo_proper _ _ _ _ _ _ _ (proj1_sig x) (proj2_sig x) i).
-     (* todo: shouldn't be necessary. perhaps an [Existing Instance] for
-       a specialization of proj2_sig is called for. *)
+   - constructor; try apply _.
+     intros x y E i A B F.
      rewrite F. apply E.
-    repeat intro...
-   intros ? ? f ? g i ? ? E.
-   simpl in *. unfold Basics.compose.
-   destruct (@homo_proper _ _ _ _ _ _ _ (proj1_sig f) (proj2_sig f) i). (* todo: clean up *)
-   destruct (@homo_proper _ _ _ _ _ _ _ (proj1_sig g) (proj2_sig g) i).
-   rewrite E...
+   - now repeat intro.
+   - intros ? ? f ? g i ? ? E.
+     now rewrite E.
   Qed.
 
-  Let hintje: ∀ x y, Equiv (object x ⟶ object y). intros. apply _. Defined. (* todo: shouldn't be necessary *)
-
   Global Instance mono: ∀ (X Y: algebras.Object sign) (a: X ⟶ Y),
-    Mono (@fmap _ _ _ TargetArrows object _ _ _ a) → (* todo: too ugly *)
-    Mono a.
+    Mono (fmap object a) → Mono a.
   Proof with simpl in *; intuition.
    repeat intro.
-   destruct a as [? [? ?]].
-   assert (fmap object f = fmap object g).
+   assert (fmap object f = fmap object g). {
     apply H.
-    repeat intro...
-    destruct f as [? [? ?]].
-    simpl in *.
-    pose proof (H0 i).
-    rewrite H1...
-   apply H1...
+    repeat intro.
+    rewrite H1.
+    apply H0.
+   }
+   now apply H1.
   Qed.
 
 End contents.
